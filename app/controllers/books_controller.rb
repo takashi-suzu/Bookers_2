ab5c3ed8abeefb31081ec67412afe1bf292d9960
takashi-suzu <t.suzuki33333@gmail.com>
2020-05-14 21:17:56 +0900

    [add]ユーザーページの追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,10 @@
+class BooksController < ApplicationController
+    def create
+    end
+    def index
+    end
+    def show
+    end
+    def update
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'top' => 'homes#top'
+  devise_for :users
+  root 'users#show'
+  resources :users, only: [:inded, :show]
+  resources :books, only: [:create, :index, :show, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
